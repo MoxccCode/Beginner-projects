c9e0282c5097c354eb484ece43f5777e2c698310
MoxccCode <moxccmail@gmail.com>
2025-01-26 15:44:05 +0200

    Progress Steps
    I hope I can finally commit something.. Finger crossed..

--- Progress Steps.js
+++ Progress Steps.js
@@ -1,1 +1,50 @@
+const progress = document.getElementById("progress");
+const circles = document.querySelectorAll(".circle");
+const next = document.getElementById("next");
+const previous = document.getElementById("previous");
+
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+    currentActive++;
+
+    if (currentActive > circles.length) {
+        currentActive = circles.length;
+    }
+
+    updateProgress();
+})
+
+previous.addEventListener("click", () => {
+    currentActive--;
+
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
+
+    updateProgress();
+})
+
+function updateProgress() {
+    circles.forEach((circle, index) => {
+        if (index < currentActive) {
+            circle.classList.add("active");
+        } else {
+            circle.classList.remove("active");
+        }
+    })
+
+    const actives = document.querySelectorAll(".active");
+
+    progress.style.width = (actives.length -1) / (circles.length - 1) * 100 + "%";
+
+    if (currentActive === 1) {
+        previous.disabled = true;
+    } else if (currentActive === circles.length) {
+        next.disabled = true;
+    } else {
+        previous.disabled = false;
+        next.disabled = false;
+    }
+}
 
